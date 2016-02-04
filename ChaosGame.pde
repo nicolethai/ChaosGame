04b081a4898a4d0c69bd3d17c6fca59226a197c7
nicolethai <ThaiNicole@gmail.com>
2016-02-04 11:37:59 -0800

    To start with instructions on Chaos Algorithm.

--- ChaosGame.pde
+++ ChaosGame.pde
@@ -2,9 +2,15 @@
 	TRY: Chaos Game	https://en.wikipedia.org/wiki/Chaos_game
 	http://www.shodor.org/interactivate/activities/TheChaosGame/
 	http://thatsmaths.com/2014/05/22/the-chaos-game/
+		Let’s specify the algorithm in a simple case. We need a dice (I should say die but that feels awkward) with three numbers. We can use a standard dice and identify 6 with 1, 5 with 2 and 4 with 3; that is, for face n we call it min(n, 7 – n)).
+			(int)(Math.random()*3)+1;
+		- Fix three points in the plane, C1, C2 and C3. For definiteness, we take the points C1 = (0, 0), C2 = (1, 0) and C3 = (0.5, √3/2), the corners of an equilateral triangle.
+		- Pick any point P0 and draw a dot there. This is our starting point. At each stage, we denote the current point by Pk and call it the game point.
+		- Roll the dice. If n comes up, draw a point half way between Pk and Cn. For example, if we roll a 2, we pick the point half way between the current point Pk and C2. This is the new game point.
+		- Repeat this procedure many times, drawing a new point at each step.
 */
 
-public int numTriangles = 20;
+// public int numTriangles = 20;
 public void setup()
 {
 	size(700, 700);
@@ -21,15 +27,19 @@ public void mouseMoved()//optional
 
 public void mouseClicked()
 {
-	numTriangles+=2;
+	// numTriangles+=2;
 }
 
 public void sierpinski(int x, int y, int len) 
 {
-	if(len > numTriangles) { 
-
+	if(len > 20) { 
+		sierpinski(x+len/2, y-len/2, len/2);
 	}
 	else {
-
+		strokeWeight(3);
+		stroke(255);
+		point(x, y);
+		point(x+len/2, y-len);
+		point(x+len, y);
 	}
 }
--- build-tmp/source/ChaosGame.java
+++ build-tmp/source/ChaosGame.java
@@ -17,9 +17,10 @@ public class ChaosGame extends PApplet {
 /*
 	TRY: Chaos Game	https://en.wikipedia.org/wiki/Chaos_game
 	http://www.shodor.org/interactivate/activities/TheChaosGame/
+	http://thatsmaths.com/2014/05/22/the-chaos-game/
 */
 
-public int numTriangles = 20;
+// public int numTriangles = 20;
 public void setup()
 {
 	size(700, 700);
@@ -36,36 +37,20 @@ public void mouseMoved()//optional
 
 public void mouseClicked()
 {
-	numTriangles+=2;
+	// numTriangles+=2;
 }
 
 public void sierpinski(int x, int y, int len) 
 {
-	if(len > numTriangles) { // len > num : num-- -> more triangles
-	// BASE CLASS : Calls Sierpinski with w & h of len x3 in different places
-	/*	triangle */
-		// sierpinski(x, y, len/2); // triangle with height and width of len/2
-		sierpinski(x+(len/2), y, len/2); // triangle next to ^
-		// sierpinski(x+(len/4), y-(len/2), len/2); // triangle on top of two ^
-	/**/
-
-	/* FOR CHAOS GAME */
-
-	/**/
+	if(len > 20) { 
+		sierpinski(x+len/2, y-len/2, len/2);
 	}
 	else {
-		// noFill();
-		// stroke((float)Math.random()*255, (float)Math.random()*255, (float)Math.random()*255);
-		// triangle(x, y, x+(len/2), y-len, x+len, y); // triangle with height and width of len
-		
-		/* FOR CHAOS GAME */
-		// stroke(255);
-	    // triangle(x, y, x+(len/2), y-len, x+len, y); // triangle with height and width of len
-	    stroke(255, 0, 0);
-	    point(x, y);
-	    point(x+len/2, y-len/2);
-	    point(x+len, y);
-		/**/
+		strokeWeight(3);
+		stroke(255);
+		point(x, y);
+		point(x+len/2, y-len);
+		point(x+len, y);
 	}
 }
   static public void main(String[] passedArgs) {
